f9d068d9a7d3081f889d220e58a03b06a841c089
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-07-21 15:57:19 +0200

    Header quasi completo

--- laravel-comics/resources/views/partials/header.blade.php
+++ laravel-comics/resources/views/partials/header.blade.php
@@ -4,6 +4,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
         <title>Laravel</title>
 
@@ -17,8 +18,88 @@
 
     <body>
         <header>
-
-        
+            <div class="backround-blue">
+                <div class="container">
+                    <div class="d-flex align-items-center justify-content-end ">
+                        <a href="#" class="text-white">
+                            <p><strong>dc power visa</strong></p>
+                        </a>
+                        <a href="#" class="text-white">
+                            <p class="mx-3"><strong>ADDITIONAL DC SITES <i class="fa-solid fa-caret-down"></i></strong></p>
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="bg-white mt-3 mb-3">
+                <div class="container">
+                    <div class="row align-items-center">
+                        <div class="col-2">
+                            <a href="/">
+                                <img src="{{Vite::asset('/resources/images/dc-logo.png')}}" alt="logo">
+                            </a>
+                        </div>
+                        <div class="col-10">
+                            <ul>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>CHARACTERS</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>COMICS</strong>    
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>MOVIES</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>TV</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>GAMES</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>COLLECTIBLES</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>VIDEOS</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>FANS</strong>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>NEWS</strong>  
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="link.active">
+                                        <strong>SHOP</strong>
+                                        <i class="fa-solid fa-caret-down"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <input type="text" class="float-end" placeholder="Search">
+                                    <i class="fa-solid fa-magnifying-glass"></i>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </header>
     </body>
 
